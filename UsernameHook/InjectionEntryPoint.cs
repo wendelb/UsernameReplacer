9e97fa5c85982c6ab6fae860864db407418aa13f
Bernhard Wendel <bwendel@gmx.de>
2017-05-06 16:10:45 +0200

    Basic HookEntry Structure
    copied from EasyHook-Tutorials

--- UsernameHook/InjectionEntryPoint.cs
+++ UsernameHook/InjectionEntryPoint.cs
@@ -6,7 +6,103 @@ using System.Threading.Tasks;
 
 namespace UsernameHook
 {
-    public class InjectionEntryPoint
+    /// <summary>
+    /// EasyHook will look for a class implementing <see cref="EasyHook.IEntryPoint"/> during injection. This
+    /// becomes the entry point within the target process after injection is complete.
+    /// </summary>
+    public class InjectionEntryPoint : EasyHook.IEntryPoint
     {
+        /// <summary>
+        /// Reference to the server interface within UsernameReplacer
+        /// </summary>
+        ServerInterface _server = null;
+
+        /// <summary>
+        /// Message queue of all files accessed
+        /// </summary>
+        Queue<string> _messageQueue = new Queue<string>();
+
+        /// <summary>
+        /// EasyHook requires a constructor that matches <paramref name="context"/> and any additional parameters as provided
+        /// in the original call to <see cref="EasyHook.RemoteHooking.Inject(int, EasyHook.InjectionOptions, string, string, object[])"/>.
+        ///
+        /// Multiple constructors can exist on the same <see cref="EasyHook.IEntryPoint"/>, providing that each one has a corresponding Run method (e.g. <see cref="Run(EasyHook.RemoteHooking.IContext, string)"/>).
+        /// </summary>
+        /// <param name="context">The RemoteHooking context</param>
+        /// <param name="channelName">The name of the IPC channel</param>
+        public InjectionEntryPoint(EasyHook.RemoteHooking.IContext context, string channelName)
+        {
+            // Connect to server object using provided channel name
+            _server = EasyHook.RemoteHooking.IpcConnectClient<ServerInterface>(channelName);
+
+            // If Ping fails then the Run method will be not be called
+            _server.Ping();
+        }
+
+        /// <summary>
+        /// The main entry point for our logic once injected within the target process.
+        /// This is where the hooks will be created, and a loop will be entered until host process exits.
+        /// EasyHook requires a matching Run method for the constructor
+        /// </summary>
+        /// <param name="context">The RemoteHooking context</param>
+        /// <param name="channelName">The name of the IPC channel</param>
+        public void Run(EasyHook.RemoteHooking.IContext context, string channelName)
+        {
+            // Injection is now complete and the server interface is connected
+            _server.IsInstalled(EasyHook.RemoteHooking.GetCurrentProcessId());
+
+            // Install hooks
+
+            // GetUserName https://msdn.microsoft.com/de-de/library/windows/desktop/ms724432(v=vs.85).aspx
+            var getUsernameHook = EasyHook.LocalHook.Create(
+                EasyHook.LocalHook.GetProcAddress("Advapi32.dll", "GetUserNameW"),
+                new GetUsername_Delegate(GetUsername_Hook),
+                this);
+
+            // Activate hooks on all threads except the current thread
+            getUsernameHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
+
+            _server.ReportMessage("GetUserName hook is installed");
+
+            // Wake up the process (required if using RemoteHooking.CreateAndInject)
+            EasyHook.RemoteHooking.WakeUpProcess();
+
+            try
+            {
+                // Loop until FileMonitor closes (i.e. IPC fails)
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(500);
+
+                    string[] queued = null;
+
+                    lock (_messageQueue)
+                    {
+                        queued = _messageQueue.ToArray();
+                        _messageQueue.Clear();
+                    }
+
+                    // Send newly monitored file accesses to FileMonitor
+                    if (queued != null && queued.Length > 0)
+                    {
+                        _server.ReportMessages(queued);
+                    }
+                    else
+                    {
+                        _server.Ping();
+                    }
+                }
+            }
+            catch
+            {
+                // Ping() or ReportMessages() will raise an exception if host is unreachable
+            }
+
+            // Remove hooks
+            getUsernameHook.Dispose();
+
+            // Finalise cleanup of hooks
+            EasyHook.LocalHook.Release();
+        }
     }
 }
